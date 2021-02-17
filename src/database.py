a26729a810fe4c73cc0ca9aafce9a7aa2e4eb753
Mason Hawver <mason.hawver@gmail.com>
2021-02-17 08:53:00 -0600

    Add files via upload

---
+++ src/database.py
@@ -0,0 +1,28 @@
+
+
+collage_meta = {
+    
+    "[Name]" : {
+        
+        "rigor" : {
+            
+            "SAT" : "{Num}",
+            "ACT" : "{Num}",
+            "GPA" : "{Num}",
+            "ACPT Rate" : "{Num}"
+        
+        }
+        
+        "local" : {
+            
+            "Adress" : "{Text}",
+            "campus" : {
+                "Type" : "{Enum : [Urban, Suburban]}",
+                "Img" : "{Text}"
+            }
+        
+        }
+    
+    }
+    
+}
