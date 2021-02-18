0492ead711b524087237c8002952cf2c32ad2fe3
Mason Hawver <mason.hawver@gmail.com>
2021-02-18 08:08:08 -0600

    Add files via upload

---
+++ src/CollageDataBase.py
@@ -0,0 +1,76 @@
+import json
+
+class Collage:
+    
+    aliases = {
+        "nick name" : "offical name",
+        "UIUC" : "University of Illinois Urban Champaighn"    
+    }
+    
+    meta_data = {
+        
+        #Cost
+        
+        #rigor of aceptance
+        "rigor" : {
+            
+            "SAT" : 0,
+            "ACT" : 0,
+            "GPA" : 0,
+            "ACPT Rate" : 0
+        
+        },
+        
+        #Reward
+        
+        #location detail
+        "local" : {
+            
+            "Adress" : "str",
+            "campus" : {
+                "Type" : "str",
+                "Img" : "str"
+            }
+        
+        }
+
+    }
+    
+    def __init__(self, name):
+        
+        if (name in Collage.aliases):
+            self.name = Collage.aliases[name]
+        else:
+            self.name = name
+        
+        self.data = Collage.meta_data
+
+
+class CollageDataBase:
+    def __init__(self):
+        self.db = {}
+    
+    def add(self, collage):
+        self.db[collage.name] = collage.data
+    
+    def load(self, file_name):
+        with open(file_name) as fp:
+            self.db = json.load(fp)
+        
+    def save(self):
+        with open('CollageDB.json', 'w') as fp:
+            json.dump(self.db, fp)
+
+
+c = Collage("gg")
+db = CollageDataBase()
+db.add(c)
+
+db.save()
+    
+
+
+
+
+
+
